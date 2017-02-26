edcb183a7a44c26f86570b8407115324793521ed
ayushig20 <20ayushigupta@gmail.com>
2017-02-26 21:23:48 +0530

    Initial commit

---
+++ ArtifactIdMavenProject/src/main/java/Main.java
@@ -0,0 +1,32 @@
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+public class Main {
+
+	public static void main(String args[]) throws Exception {
+		System.out.println("MySQL Connector Sample Project");
+		
+		//Loading driver
+		Class.forName("com.mysql.jdbc.Driver");
+		
+		//Creating connection object
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/FirstDatabase", "root", "admin");
+		
+		//Creating SQL query
+		PreparedStatement ps = con.prepareStatement("Select * from Register");
+
+		//Executing SQL query
+		ResultSet rs = ps.executeQuery();
+		
+		//Iterating 
+		while (rs.next()) {
+			System.out.println("Name: " + rs.getString(1) + "\nPhone: " + rs.getString(2));
+		}
+
+		System.out.println("Hitted database successfully");
+	}
+}
